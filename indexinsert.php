8e884628226077c4002eaaafdeea2b71e395ece3
amsmite001 <smite.am@outlook.com>
2019-07-24 13:49:27 -0600

    Fix to styling

--- indexinsert.php
+++ indexinsert.php
@@ -1,30 +1,26 @@
 <?php
 $host = "localhost";
 $dbusername = "root";
-$dbpassword = "kcp3pp3r";
+$dbpassword = "";
 $dbname = "trip";
 $conn = mysqli_connect($host,$dbusername,$dbpassword);
 if (!$conn) {
-   echo "no";
-    echo('Connect Error (' . mysqli_connect_errno() . ')' . mysqli_connect_error());
+    die('Connect Error (' . mysqli_connect_errno() . ')' . mysqli_connect_error());
 }
 if (!mysqli_select_db($conn, 'travelTracker')) {
-    echo "issue with db";
+    echo "Issue with db";
 }
 else {
     $start = $_POST['startDate_InputFields'];
     $end = $_POST['endDate_InputFields'];
     $dest = $_POST['destinations'];
-    $status = 'W';
-    echo "wth";
     
-    $sql = "INSERT INTO trip (startDate, endDate, destination, stat) VALUES ('$start', '$end', '$dest', 'A')";
+    $sql = "INSERT INTO trip (startDate, endDate, destination, stat) VALUES ('$start', '$end', '$dest', 'N')";
     if (mysqli_query($conn,$sql)) {
         echo "New Record is inserted.";
     }
     else {
-        echo "error: ";
-    }
+        die('Error: ' . mysql_error());
 }
 
 mysql_close();
